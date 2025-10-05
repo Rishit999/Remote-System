9bc3b55fdc4ee4a0c27a526ee6ca3a233b262df9
Rishit999 <rishitnayakar@gmail.com>
2025-10-05 23:17:29 +0530

    Create addsub.py

---
+++ addsub.py
@@ -0,0 +1,9 @@
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+
+addition = num1 + num2
+subtraction = num1 - num2
+
+print(f"\nResults:")
+print(f"Addition of {num1} and {num2} = {addition}")
+print(f"Subtraction of {num1} and {num2} = {subtraction}")
